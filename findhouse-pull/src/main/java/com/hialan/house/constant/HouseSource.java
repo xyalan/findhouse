51b6c73b9bc8e84bb451451a867da8316090e23b
Alan <alanlhy@gmail.com>
2015-03-12 00:20:20 +0800

    added source of house

---
+++ findhouse-pull/src/main/java/com/hialan/house/constant/HouseSource.java
@@ -0,0 +1,12 @@
+package com.hialan.house.constant;
+
+/**
+ * User: Alan
+ * Email:alan@hialan.com
+ * Date: 3/12/15 00:13
+ */
+public final class HouseSource {
+	public static final String DOUBAN = "http://www.douban.com/group/shanghaizufang/";
+
+	public static final String COM_58 = "http://sh.58.com/chuzu/";
+}
