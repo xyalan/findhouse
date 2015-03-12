e70b7fd7b35bba7c5e70019d4df04fbcff17a2fe
Alan <alanlhy@gmail.com>
2015-03-12 13:08:39 +0800

    added starter test

---
+++ findhouse-pull/src/test/java/com/hialan/house/spider/SpiderStarterTest.java
@@ -0,0 +1,17 @@
+package com.hialan.house.spider;
+
+import org.junit.Test;
+/**
+ * User: Alan
+ * Email:alan@hialan.com
+ * Date: 3/12/15 10:07
+ */
+public class SpiderStarterTest {
+
+
+	@Test
+	public void testAction() {
+		SpiderStarter spiderStarter = new SpiderStarter();
+		spiderStarter.action();
+	}
+}
