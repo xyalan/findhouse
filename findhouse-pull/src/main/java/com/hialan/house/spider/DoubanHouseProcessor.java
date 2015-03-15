2cd69561b5cba98ccf57d4d8e460c65454b612fe
Alan <alanlhy@gmail.com>
2015-03-15 23:14:39 +0800

    set useragents for douban

--- findhouse-pull/src/main/java/com/hialan/house/spider/DoubanHouseProcessor.java
+++ findhouse-pull/src/main/java/com/hialan/house/spider/DoubanHouseProcessor.java
@@ -12,16 +12,20 @@ import java.util.List;
  * Date: 3/12/15 00:23
  */
 public class DoubanHouseProcessor implements PageProcessor {
-	private Site site = new Site();
+	private Site site;
 
 	@Override
 	public void process(Page page) {
-		List<String> links = page.getHtml().links().regex(".*article.*").all();
+		List<String> links = page.getHtml().links().regex(".*topic.*").all();
 		System.out.println(links);
 	}
 
 	@Override
 	public Site getSite() {
+		if (site == null) {
+			site = Site.me().setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) " +
+					"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36");
+		}
 		return site;
 	}
 
