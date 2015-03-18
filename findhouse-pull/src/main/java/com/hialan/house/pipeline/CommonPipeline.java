163c7ca3aa575ee0e65fb6e1ef018379b6ec83e5
Alan <alanlhy@gmail.com>
2015-03-18 22:56:50 +0800

    added handling result

---
+++ findhouse-pull/src/main/java/com/hialan/house/pipeline/CommonPipeline.java
@@ -0,0 +1,17 @@
+package com.hialan.house.pipeline;
+
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.pipeline.Pipeline;
+
+/**
+ * User: Alan
+ * Email:alan@hialan.com
+ * Date: 3/18/15 22:54
+ */
+public class CommonPipeline implements Pipeline {
+	@Override
+	public void process(ResultItems resultItems, Task task) {
+
+	}
+}
